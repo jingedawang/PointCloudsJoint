167daacc29790b1d066c88397cf2f5cf18a93d69
jingedawang <wjg172184@163.com>
2017-05-29 11:10:58 +0800

    First version.

---
+++ cmake-build-debug/CMakeFiles/PointCloudsJoint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointCloudsJoint.dir/PointCloudsJoint.cpp.o"
+  "PointCloudsJoint.pdb"
+  "PointCloudsJoint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointCloudsJoint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
